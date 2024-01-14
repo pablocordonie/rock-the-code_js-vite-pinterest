864fc3e8c9b9844ba02f6f0e26c187431d109ad1
pablocordonie <pablogarcia.93.06@gmail.com>
2024-01-14 13:37:20 +0100

    feat: create a first layout design using flex props in main.css & optimize a bit the header layout in header.css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 import renderHeader from './components/Header/header';
-//import renderMain from './components/Main/main';
+import renderMain from './components/Main/main';
 
 renderHeader();
-//renderMain(import.meta.env.VITE_KEY);
+renderMain(import.meta.env.VITE_KEY);
--- components/Main/main.js
+++ components/Main/main.js
@@ -1,15 +1,26 @@
+import './main.css'
 
 const renderMain = async (key) => {
     const main = document.querySelector('#app > main');
+    const imagesList = document.createElement('ul');
+    imagesList.className = 'rtc-main--images_list';
+
     const images = await fetch(`https://api.unsplash.com/photos/random?count=10&client_id=${key}`).then((res) => res.json()).then((res) => {
         console.log(res);
         for (const image of res) {
+            const imgLi = document.createElement('li');
+            imgLi.className = 'rtc-main--images-item';
+
             const img = document.createElement('img');
             img.src = `${image.urls.regular}`;
             img.alt = `${image.alt_description}`;
             img.className = `rtc-main--${image.user.username}_img`;
+            img.classList.add('rtc-main--images-item-img');
             img.setAttribute('loading', 'lazy');
-            main.appendChild(img);
+
+            imgLi.appendChild(img);
+            imagesList.appendChild(imgLi);
+            main.appendChild(imagesList);
         }
     }).catch(error => console.log(error));
 
